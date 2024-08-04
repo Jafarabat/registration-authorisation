0534aca5d35daf90f409bf098cd8c60fa08b4306
Jafarabat <137901795+Jafarabat@users.noreply.github.com>
2024-08-04 22:11:20 +0300

    Delete Models directory

--- Models/PasswordHasher.cs
+++
@@ -1,9 +0,0 @@
-
-namespace JWT.Models;
-public class PasswordHasher
-{
-    public static string HashPassword(string password)
-    {
-        return BCrypt.Net.BCrypt.HashPassword(password);
-    }
-}
